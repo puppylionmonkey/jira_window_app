1e0b68dc956f0e359ad855d5399d4d6119afc072
puppylionmonkey <puppylionmonkey33@gmail.com>
2026-02-04 18:51:56 +0800

    revised 沒有result

--- export_test_from_jira/get_excel.py
+++ export_test_from_jira/get_excel.py
@@ -7,23 +7,23 @@ from export_test_from_jira.repo import test_id_lipped_repo_dict
 from path_config import project_path
 
 
-def jira_test_csv_to_import_csv(issue_id, account, jira_api_token):
+def jira_test_csv_to_import_csv(issue_id, account='', jira_api_token=''):
     # --- 自動獲取 Windows 使用者的下載資料夾 ---
     downloads_path = Path.home() / "Downloads"
     # 組合完整的儲存路徑
     final_save_path = downloads_path / f"{issue_id}_importer.csv"
 
-
     target_columns = [
         'Test Repo', 'Issue Id', 'Issue key', 'Test type', 'Test Summary',
-        'Test Priority', 'Action', 'Data', 'Result', 'Links',
+        'Test Priority', 'Action', 'Data', 'Expected Result', 'Links',
         'Description', 'Unstructured', 'definition'
     ]
 
-
     df = pd.read_csv(f'{downloads_path}/{issue_id}.csv')
+    print(df['Expected Result'], 'abc1')
     df = df.reindex(columns=target_columns)
     df = df.astype(object)  # 強制整張表轉為 object 型別
+    print(df['Expected Result'], 'abc2')
 
     df = df.rename(columns={
         'Action': 'Action',
@@ -31,9 +31,9 @@ def jira_test_csv_to_import_csv(issue_id, account, jira_api_token):
         'Expected Result': 'Result'
     })
     df = df.reindex(columns=target_columns)
+    print(df['Result'], 'abc3')
 
-
-    test_summary= get_test_summary_from_jira_api(issue_id, account, jira_api_token)
+    test_summary = get_test_summary_from_jira_api(issue_id, account, jira_api_token)
     story_list = get_issue_links(issue_id, account, jira_api_token)
 
     df.at[0, 'Test Repo'] = test_id_lipped_repo_dict.get(issue_id, '')
@@ -43,3 +43,6 @@ def jira_test_csv_to_import_csv(issue_id, account, jira_api_token):
     df.at[0, 'Test Priority'] = 'Medium'
     df.at[0, 'Links'] = ';'.join(story_list)
     df.to_csv(final_save_path, index=False)
+
+
+jira_test_csv_to_import_csv(issue_id='PBPM-25818')
